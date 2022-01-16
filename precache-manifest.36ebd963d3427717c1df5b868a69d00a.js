cbc0801430fb2288b01b15307684c6b1f5fc3976
Vladymyr Antonenko <vladymyr.a13@gmail.com>
2022-01-16 03:01:17 +0200

    Updates

---
+++ precache-manifest.36ebd963d3427717c1df5b868a69d00a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c88f0d51df2c9f437229dbac827c61",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "8587a71d3761c05a88d5",
+    "url": "/goit-react-hw-02-feedback/static/css/main.61be4807.chunk.css"
+  },
+  {
+    "revision": "391767d4e6134371753c",
+    "url": "/goit-react-hw-02-feedback/static/js/2.aa938ad9.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/goit-react-hw-02-feedback/static/js/2.aa938ad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8587a71d3761c05a88d5",
+    "url": "/goit-react-hw-02-feedback/static/js/main.fced7c9e.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
