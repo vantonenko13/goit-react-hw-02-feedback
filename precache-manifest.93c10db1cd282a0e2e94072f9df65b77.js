16c0a10e725ff452e54331015cedaf704f19e27d
Vladymyr Antonenko <vladymyr.a13@gmail.com>
2022-01-22 15:59:11 +0200

    Updates

--- precache-manifest.93c10db1cd282a0e2e94072f9df65b77.js
+++ precache-manifest.93c10db1cd282a0e2e94072f9df65b77.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fb477d95d5d69ba7de3ab09e35a0dcfc",
+    "revision": "561d432cf225b4778ae3ebdf3cfe3f55",
     "url": "/goit-react-hw-02-feedback/index.html"
   },
   {
-    "revision": "44a198301916e5b028a2",
+    "revision": "889a9f1a9fd6dbab4d28",
     "url": "/goit-react-hw-02-feedback/static/css/main.61be4807.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-02-feedback/static/js/2.901f605a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "44a198301916e5b028a2",
-    "url": "/goit-react-hw-02-feedback/static/js/main.eebba351.chunk.js"
+    "revision": "889a9f1a9fd6dbab4d28",
+    "url": "/goit-react-hw-02-feedback/static/js/main.3cb54eeb.chunk.js"
   },
   {
     "revision": "cb08e2ec47267fd4ee9f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-feedback/precache-manifest.26b0c6324955d0ca178c9b3c669d4b3d.js"
+  "/goit-react-hw-02-feedback/precache-manifest.93c10db1cd282a0e2e94072f9df65b77.js"
 );
 
 self.addEventListener('message', (event) => {
